83a03126b3a652206969d7bb8e4b9cae348b6c61
Subramanyam-r <subramanyam.r.2001@gmail.com>
2021-05-27 23:10:27 +0530

    v1.0.0 - Init commit

---
+++ Menu.js
@@ -0,0 +1,43 @@
+const menu = [
+    {
+        name: "Idli",
+        img: "https://media.istockphoto.com/photos/idli-with-coconut-chutney-and-sambhar-picture-id638506124?k=6&m=638506124&s=612x612&w=0&h=jmbVHMXsTJ6GhquYnAss_ITYNKL2Q0x7jAGkdWI6liU=",
+        available: 10,
+        waitingTime: 5,
+        price: 10
+    },
+
+    {
+        name: "Dosa",
+        img: "https://im.whatshot.in/img/2020/Nov/12441-d-1605777807.jpg",
+        available: 10,
+        waitingTime: 5,
+        price: 40
+    },
+
+    {
+        name: "Chola Poori",
+        img: "http://1.bp.blogspot.com/-h_nEExmq8_I/UA6hIiony1I/AAAAAAAAAKw/e3TYTKkN-Tg/s1600/323042_163698697069002_151120571660148_220154_776677215_o.jpg",
+        available: 15,
+        waitingTime: 6,
+        price: 70
+    },
+    
+    {
+        name: "Chapati",
+        img: "https://thumbs.dreamstime.com/b/indian-food-chapati-chicken-24484160.jpg",
+        available: 20,
+        waitingTime: 3,
+        price: 30
+    },
+
+    {
+        name: "Vada",
+        img: "https://img-global.cpcdn.com/recipes/3d1c5281876a8126/751x532cq70/medu-vada-recipe-main-photo.jpg",
+        available: 30,
+        waitingTime: 1,
+        price: 25
+    }
+];
+
+module.exports = menu;
---
+++ app.js
@@ -0,0 +1,125 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+require('dotenv').config();
+let menu = require("./menu");
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+const cors = require("cors");
+
+corsOptions={
+    cors: true,
+   }
+const io = require("socket.io")(server, corsOptions);
+const PORT = process.env.PORT || 3001;
+app.use(cors());
+
+let tables = ["FREE", "FREE", "FREE", "FREE", "FREE", "FREE"];
+let menuRooms = ["", "", "", "", "", ""];
+let menusockets = ["", "", "", "", "", ""];
+let orders = [];
+
+app.get("/", (req, res) => {
+
+    res.send("Checkup: Balajee Bhavan backend is up!");
+});
+
+io.on("connection", socket => {
+    console.log("A new customer connected!  |  " + socket.id);
+    socket.emit("tableUpdate", {tables});
+
+    socket.on("sendotp", ({phno, table}, callback) => {
+        client.verify.services('VA58729189f567cadecbcc90d4931cb24d')
+             .verifications
+             .create({to: "+91"+phno, channel: 'sms'})
+             .then(verification => {
+                 console.log(verification.sid);
+             });
+
+        menuRooms[parseInt(table)-1] = phno;
+        callback();
+    });
+
+    socket.on("gettables", (callback) => {
+        callback(tables);
+    });
+
+    socket.on("joinedmenu", (table, callback) => {
+        let room = "menu-" + table;
+        socket.join(room);
+        console.log(`joined: menu-${table}`);
+        tables[parseInt(table)-1] = "BOOKED";
+        menusockets[parseInt(table)-1] = socket.id;
+        socket.broadcast.emit("tableUpdate", {tables});
+        callback(menu);
+    });
+
+    socket.on("joinrestaurant", (callback) => {
+        let room = "restaurant";
+        socket.join(room);
+        callback(orders, menu);
+    });
+
+    socket.on("orderplaced", (itemName, nos, table, callback) => {
+        orders.push({
+            table: parseInt(table),
+            item: itemName,
+            status: "Pending Confirmation",
+            nos: parseInt(nos),
+            phno: menuRooms[parseInt(table) - 1]
+        });
+        menu.find(item => item.name === itemName).available -= nos;
+        io.in("restaurant").emit("ordersupdate", orders, menu);
+        io.emit("menuupdate", menu);
+        socket.emit("yourOrder", orders.filter(order => order.table === parseInt(table)));
+    });
+
+    socket.on("orderstatusupdate", (index, status) => {
+        orders[index].status = status;
+        let table = orders[index].table;
+        socket.to(`menu-${table}`).emit("orderupdate", orders.filter(order => order.table === table));
+        io.in("restaurant").emit("ordersupdate", orders, menu);
+    });
+
+    // socket.on("leftmenu", (table) => {
+    //     console.log("leftenu")
+    //     tables[parseInt(table)-1] = "FREE";
+    //     socket.broadcast.emit("tableUpdate", {tables});
+    //     orders = orders.filter(order => order.table !== parseInt(table));
+    //     io.in("restaurant").emit("ordersupdate", orders);
+    // });
+
+    socket.on("disconnect", () => {
+        if(menusockets.findIndex(order => socket.id) !== -1) {
+            let table = menusockets.findIndex(order => order === socket.id)+1;
+            tables[parseInt(table)-1] = "FREE";
+            socket.broadcast.emit("tableUpdate", {tables});
+            orders = orders.filter(order => order.table !== parseInt(table));
+            menusockets[parseInt(table)-1] = "";
+            menuRooms[parseInt(table)-1] = "";
+
+            io.in("restaurant").emit("ordersupdate", orders, menu);
+        }
+        console.log("User Disconnected: " + socket.id);
+    });
+
+    socket.on("restaurantmenuupdate", (newMenu => {
+        menu = newMenu;
+        io.emit("menuupdate", menu);
+    }))
+
+    socket.on("validateotp", ({otp, table, phno}, callback) => {
+        client.verify.services('VA58729189f567cadecbcc90d4931cb24d')
+      .verificationChecks
+      .create({to: "+91"+phno, code: otp})
+      .then(verification_check => {
+          callback(verification_check.status === "approved");
+      }).catch(err => { if(err) callback(false) });
+    });
+});
+
+server.listen(PORT, () => {
+    console.log("Server is running on PORT " + PORT);
+});
